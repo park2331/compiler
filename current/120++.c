80961ef91c664981b7e2c69511e1086d6fdf5c18
Richard Park <richardpark@Richards-MacBook-Pro.local>
2014-10-24 21:12:41 -0700

    Cleaned up and reformatted

---
+++ current/120++.c
@@ -0,0 +1,69 @@
+/* 120++.c
+ * Main file for lexer
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "token.h"
+#include "tlist.h"
+#include "120lex.h"
+#include "tree.h"
+#include "120gram.tab.h"
+
+extern FILE *yyin;
+extern char *yytext;
+extern tokenptr yytoken;
+extern int yyparse();
+extern YYSTYPE yylval;
+extern treeptr t_unit;
+
+nodeptr filestack = NULL;
+
+int main(int argc, char **argv) {
+
+  struct node *root = NULL;
+
+  int i;
+  int code = -1;
+  const char *filelist[argc];
+
+  if(argc == 1) {
+    printf("No files to (F)lex!\n");
+    return 0;
+  }
+  /* Put file names in list */
+  argc--;
+  argv++;
+  for (i = 0; i < argc; i++) {
+    filelist[i] = argv[i];
+  }
+
+  /* Push onto flex's convenient buffer */
+  for (i = 0; i < argc; i++) {
+    yyin = fopen(filelist[i],"r");
+    if (yyin == NULL) {
+      printf("%s is not a file!\n",filelist[i]);
+      return 0;
+    }
+
+    /* Push onto filestack as defined in token.c */
+    push_node(&filestack, filelist[i]);
+    yypush_buffer_state(yy_create_buffer(yyin, YY_BUF_SIZE));
+  }
+
+  printf("FILES TO BE PARSED:\n");
+  print_stack(&filestack);
+    
+  int parse_code = 0;
+
+  
+  while(!feof(yyin)) {
+      parse_code = yyparse();
+
+    }
+  
+  printf("Result: %d\n", parse_code);
+  //      printf("%s\n",yylval.tptr->name);
+  treeprint(t_unit, 0);
+  return 0;
+}
---
+++ current/tlist.c
@@ -0,0 +1,79 @@
+/* tlist.c
+ * Linked List data structure source.
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "tlist.h"
+#include "token.h"
+
+void print_stack(nodeptr *top) {
+  if(*top == NULL) {
+    printf("EMPTY STACK\n");
+    
+  } else {
+    nodeptr temp;
+    temp = *top;
+    while (temp != NULL) {
+      printf("%s\n",temp->filename);
+      temp = temp->next;
+    }
+  }
+}
+
+void pop_node(nodeptr *top) {
+  nodeptr temp;
+  temp = *top;
+  *top = (*top)->next;
+  free(temp);
+}
+
+/* Push node on char* stack for filenames */
+void push_node(nodeptr *top, char *fname) {
+  nodeptr temp = (nodeptr)malloc(sizeof(struct node));
+  temp->filename = malloc(strlen(fname)+1);
+  temp->next = NULL;
+  strcpy(temp->filename, fname);
+
+  if(top == NULL) {
+    *top = temp;
+  } else {
+    temp->next = *top;
+    *top = temp;
+  }
+}
+
+/* Add token to linked list */
+void add_node(nodeptr *head, tokenptr value) {
+  nodeptr a = malloc(sizeof(struct node));
+  a->t = value;
+  a->next = NULL;
+  if (*head == NULL) {
+    *head = a;
+  } else {
+    nodeptr go = *head;
+    while (go->next != NULL) {
+      go = go->next;
+    }
+    go->next = a;
+  }
+}
+
+void print_nodes(nodeptr *head) {
+  nodeptr go = *head;
+  while (go != NULL) {
+    //    printf("TOKEN: %d\n",go->t->category);
+    print_token(go->t);
+    go = go->next;
+  }
+}
+
+void delete_all_nodes(nodeptr *head) {
+  nodeptr go = *head;
+  while (go != NULL) {
+    nodeptr del = go;
+    go = go->next;
+    free(del);
+  }
+}
